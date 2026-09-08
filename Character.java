eea4ba423b41625e19e51ab194c2b74ce75348b3
Syuki <syukiplay@gmail.com>
2026-09-08 16:26:16 -0300

    improve battle system

--- Character.java
+++ Character.java
@@ -14,10 +14,30 @@
         int damage;
 
         //mechanics
-        public void attack(Character that) { 
-            that.hp = this.damage(this.strength) - that.hp;
-            System.out.println(this.name + " has attacked!");
+        public void attack(Character that) throws InterruptedException { 
+            that.hp = that.hp - (this.strength);
+            System.out.println(this.name + " has attacked! ");
+            Thread.sleep(1000);
+
+            System.out.println(that.name + " takes " + this.strength  + " damage!");
+            Thread.sleep(1000);
+
+            System.out.println(that.name + " is " + that.hp + " HP left!");
+            Thread.sleep(1000);
+            
+                if (that.hp >= 0) {
+                    System.out.println(that.name + " will revide! ");
+                    Thread.sleep(1000);  
+                    that.attack(this);
+                }
+                else {
+                    System.err.println(that.name + " has been defeated!");
+                    Thread.sleep(1000);
+                    System.err.println("Good job " + this.name + "! ");
+                }
+                
         }
+        
         public void run(Character that) {
 
             if (this.agility >= that.agility) {
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+public static void main(String[] args) throws InterruptedException {
 
     Scanner scanner = new Scanner(System.in);
     Character player = new Character();
@@ -32,61 +32,78 @@ public class Main {
 
     if (player.age <= 18) {
         System.out.println("Starting at that age, " + player.name + "? That's brave!");
+        Thread.sleep(1000);
     }
     else {
         System.out.println("You know it's time when it's the time right, " + player.name + "? All right!");
+        Thread.sleep(1000);
     }
 
-    System.out.print("Which of these elements are you most connected to?\nFire\nThunder\nIce\nWater\nGrass\nAnswer: ");
+    System.out.print("Which of these elements are you most connected to?\nFire\nThunder\nIce\nWater\nGrass\n " + "\nAnswer: ");
     player.element = scanner.nextLine(); 
 
     if (player.element.equalsIgnoreCase("Fire")) {
         System.out.println("Oh a hothead huh, All right, it fits you!");
+        Thread.sleep(1000);
     }
     else if (player.element.equalsIgnoreCase("Thunder")) {
         System.out.println("Ok! I got it! Calm down! You're so hiperactive!");
+        Thread.sleep(1000);
     }
     else if (player.element.equalsIgnoreCase("Ice")) {
         System.out.println("Okay? I thought you would be happier saying that.");
+        Thread.sleep(1000);
     }
     else if (player.element.equalsIgnoreCase("Water")) {
         System.out.println("The way your words flow from you... it fits you!");
+        Thread.sleep(1000);
     }
     else if (player.element.equalsIgnoreCase("Grass")) {
         System.out.println("... Can you be quicker answering the next?");
+        Thread.sleep(1000);
     }
     else {
         System.out.println("Uh? Never heard of it.");
+        Thread.sleep(1000);
     }
 
-    System.out.print("Which one of these describes you better?\nWarrior\nMage\nRogue\nAnswer: " );
+    System.out.print("Which one of these describes you better?\nWarrior\nMage\nRogue\n " + "\nAnswer: " );
     player.path = scanner.nextLine();
+    Thread.sleep(1000);
 
     // WARRIOR COMBINATIONS
 
     if (player.path.equalsIgnoreCase("Warrior") && player.element.equalsIgnoreCase("Thunder")) {
         System.out.println("\n!THOR COMBINATION ACTIVATED!\n" + "\nThunder Element + Warrior combination amplified your Thunder skills that uses an Axe!!!");    
+        Thread.sleep(1000);
     }
     else if (player.path.equalsIgnoreCase("Warrior")) {
         System.out.println("A strong one huh? I can see you'll be a great " +  player.element + " Warrior someday");
+        Thread.sleep(1000);
     }
     else if (player.path.equalsIgnoreCase("Mage") && player.element.equalsIgnoreCase("Fire")) {
         System.out.println("\n!PIROMANCER COMBINATION ACTIVATED!\n" + "\nFire Element + mage combination amplified your Fire skills that uses a Staff!!!");    
+        Thread.sleep(1000);
     }
     else if (player.path.equalsIgnoreCase("Mage")) {
         System.out.println("I can see... you're one of these heavy brains huh? You'll be a splendous " +  player.element + " mage!");
+        Thread.sleep(1000);
     }
     else if (player.path.equalsIgnoreCase("Rogue")) {
         System.out.println("Hm... A Rogue one... I didn't expected it but okay so, it's your choice after all. ");
+        Thread.sleep(1000);
     }
     else {
         System.out.println("I've never heard about this path...");
+        Thread.sleep(1000);
     }
 
-    System.out.print("\nLook at your left side, which of these weapons would you like to use?\nAxe\nSword\nBow\nTwin\nKnifes\nStaff\nAnswer: ");
+    System.out.print("\nLook at your left side, which of these weapons would you like to use?\nAxe\nSword\nBow\nTwin\nKnifes\nStaff\n" + "\nAnswer: ");
+    Thread.sleep(1000);
     player.weapon = scanner.nextLine();
 
     System.out.println(player.weapon + "? That's a good choice!\nNow let's see your status?");
+    Thread.sleep(1000);
     
     // Player status
     
@@ -95,20 +112,20 @@ if (player.path.equalsIgnoreCase("Warrior")) {
 player.strength = 10;
 player.agility = 4;
 player.magic = 1;
-player.hp = 10;
+player.hp = 25;
 }
 
 else if (player.path.equalsIgnoreCase("Mage")) { 
 player.strength = 0; 
 player.agility = 5; 
 player.magic = 10; 
-player.hp = 5;   
+player.hp = 15;   
 }
 else if (player.path.equalsIgnoreCase("Rogue")) {
 player.strength = 4;
 player.agility = 10;
 player.magic = 5;
-player.hp = 7;
+player.hp = 20;
 }
 
     //Weapon combinations
@@ -138,24 +155,32 @@ player.hp = 7;
 
     if (player.path.equalsIgnoreCase("Warrior")) {
     System.out.println("Your status are very good for a beginner Warrior!\n" + "\n" + player.name + " status:\n" + "Age: " + player.age + "\n" + "\n"+ "Path: "  + player.path +"\nElement: " + player.element +"\nWeapon: " + player.weapon + "\n" + "\n" + "Strength = " + player.strength + "\nAgility = " + player.agility +"\nMagic = " + player.magic +"\nHP= " + player.hp  );
+    Thread.sleep(1000);
     }
     else if (player.path.equalsIgnoreCase("Rogue")) {
     System.out.println("Your status are very good for a beginner Rogue!\n" + "\n" + player.name + " status:\n" + "Age: " + player.age + "\n" + "\n"+ "Path: "  + player.path +"\nElement: " + player.element +"\nWeapon: " + player.weapon + "\n" + "\n" + "Strength = " + player.strength + "\nAgility = " + player.agility +"\nMagic = " + player.magic +"\nHP= " + player.hp  );
+    Thread.sleep(1000);
     }
     else if (player.path.equalsIgnoreCase("Mage")) {
     System.out.println("Your status are very good for a beginner Mage!\n" + "\n" + player.name + " status:\n" + "Age: " + player.age + "\n" + "\n"+ "Path: "  + player.path +"\nElement: " + player.element +"\nWeapon: " + player.weapon + "\n" + "\n" + "Strength = " + player.strength + "\nAgility = " + player.agility +"\nMagic = " + player.magic +"\nHP= " + player.hp  );
+    Thread.sleep(1000);
     }
 
 System.out.println("\nOh and look! You have a chance to test this new powers! There's a goblin horde coming to attack the village!");
-System.out.print(goblin.name + " has appeared!\nWhat will you do?\nAttack\nRun\nAnswer: ");
+Thread.sleep(1000);
+System.out.print(goblin.name + " has appeared!\nWhat will you do?\n" + "\nAttack\nRun\n" + "\nAnswer: ");
 player.action = scanner.nextLine();
      
 if (player.action.equalsIgnoreCase("Attack")) {
-    player.attack();
+    player.attack(goblin);
+    Thread.sleep(1000);
 }
 else if (player.action.equalsIgnoreCase("Run")) {
-    player.run();
-}
+    player.run(goblin);
+} 
+
+
+
 
 scanner.close();
     }
